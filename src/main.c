4a27a736e47ea924ce7220f1cedb7c51fe46b947
Arden Rasmussen <ardenisthebest@gmail.com>
2018-12-22 15:41:03 -0800

    Updated compile commands

--- src/main.c
+++ src/main.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "entis.h"
 
 void append(char* s, char c) {
-        int len = strlen(s);
-        s[len] = c;
-        s[len+1] = '\0';
+  int len = strlen(s);
+  s[len] = c;
+  s[len + 1] = '\0';
 }
 
 int main(int argc, char* argv[]) {
   entis_init("Entis", 500, 500, 0, NULL);
   entis_set_background(ENTIS_BLACK);
-  /* entis_load_font("-*-fixed-bold-*-*-*-18-*-*-*-*-*-*-*"); */
-  entis_load_xfont(entis_load_xfont_fmt("clean", "*", '*', 12));
-  entis_load_font("scpnfcm.ttf");
+  entis_load_font("ps2p.ttf");
   entis_set_font_size(16, 0);
   entis_clear();
   entis_set_font_color(ENTIS_WHITE);
-  entis_set_font_background(ENTIS_GREEN);
-  entis_set_font_background(ENTIS_BLACK);
   entis_event event = entis_poll_event();
   uint16_t x = 0, y = 18;
   char str[255];
@@ -32,13 +28,15 @@ int main(int argc, char* argv[]) {
         if (event.key.keycode != KEY_ESCAPE) {
           append(str, event.key.keysym);
           entis_draw_text(x, y, str);
-        }else if(event.key.keycode== KEY_ESCAPE){
+        } else {
           break;
         }
       } else if (event.type == ENTIS_BUTTON_RELEASE) {
         x = event.button.x;
         y = event.button.y;
-        entis_segment(x,y,x+200,y);
+        entis_set_font_size(20 * y / 500.0, 0);
+        printf("(%dpt)\n", (int)(20 * y / 500.0));
+        entis_segment(x, y, x + 200, y);
         str[0] = 0;
       }
       event = entis_poll_event();
@@ -46,7 +44,6 @@ int main(int argc, char* argv[]) {
     entis_clear_events();
     entis_sleep(0.1);
   }
-  entis_save_png("Testing.png");
   entis_term();
   return 0;
 }
