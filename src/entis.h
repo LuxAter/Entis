1a7279c7256b675c5e10dde87501765aef82c29e
Arden Rasmussen <ardenisthebest@gmail.com>
2019-01-10 11:13:14 -0800

    Fixed filled circle

--- src/entis.h
+++ src/entis.h
@@ -673,7 +673,7 @@ void entis_rectangle_fill(uint32_t x, uint32_t y, uint32_t width,
  * @param cy Y position of the center of the circle.
  * @param r Radius of the circle in pixels.
  */
-void entis_circle_fill(uint32_t cx, uint32_t cy, uint32_t r);
+void entis_circle_fill(uint32_t cx, uint32_t cy, int32_t r);
 /**
  * @brief Draws a filled arbitrary polygon
  *
